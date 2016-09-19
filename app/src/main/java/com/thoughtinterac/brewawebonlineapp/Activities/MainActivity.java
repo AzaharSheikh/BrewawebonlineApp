a38eb5b417c0bbf79df4fb9ea259ca061973f0d8
AzaharSheikh <azaharsheikh786@gmail.com>
2016-09-19 18:03:52 +0530

    new cmt

--- app/src/main/java/com/thoughtinterac/brewawebonlineapp/Activities/MainActivity.java
+++ app/src/main/java/com/thoughtinterac/brewawebonlineapp/Activities/MainActivity.java
@@ -33,6 +33,7 @@ public class MainActivity extends AppCompatActivity
 //hiii azhar
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        //hi as
     }
 
     @Override
