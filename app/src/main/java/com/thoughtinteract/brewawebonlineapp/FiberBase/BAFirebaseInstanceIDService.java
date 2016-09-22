2a7eb3400121e6e5c177bcceb21ac192fb1ddf4f
AzaharSheikh <azaharsheikh786@gmail.com>
2016-09-22 14:57:39 +0530

    navigation updated

---
+++ app/src/main/java/com/thoughtinteract/brewawebonlineapp/FiberBase/BAFirebaseInstanceIDService.java
@@ -0,0 +1,30 @@
+package com.thoughtinteract.brewawebonlineapp.FiberBase;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by AzaharSheikh on 22-09-2016.
+ */
+public class BAFirebaseInstanceIDService extends FirebaseInstanceIdService {
+
+    private static final String TAG = "FirebaseIIDService";
+
+    @Override
+    public void onTokenRefresh() {
+
+        //Getting registration token
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+
+        //Displaying token on logcat
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+    }
+
+    private void sendRegistrationToServer(String token) {
+        //You can implement this method to store the token on your server
+        //Not required for current project
+    }
+}
