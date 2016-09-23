02ea97de9ebe7b77384bbbc8419af69768282003
AshwiniBadgujar <ashwinibadgujar3@gmail.com>
2016-09-23 12:49:48 +0530

    java2

---
+++ app/src/main/java/com/thoughtinteract/brewawebonlineapp/Activities/AppCompatActivity.java
@@ -0,0 +1,7 @@
+package com.thoughtinteract.brewawebonlineapp.Activities;
+
+/**
+ * Created by AshwiniBadgujar on 23-09-2016.
+ */
+public class AppCompatActivity {
+}
---
+++ app/src/test/java/com/thoughtinteract/brewawebonlineapp/Activities/MainActivityTest.groovy
@@ -0,0 +1,7 @@
+package com.thoughtinteract.brewawebonlineapp.Activities
+
+/**
+ * Created by AshwiniBadgujar on 23-09-2016.
+ */
+class MainActivityTest extends groovy.util.GroovyTestCase {
+}
